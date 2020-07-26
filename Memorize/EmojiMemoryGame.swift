e299ec46654967502b04aa55d45c8cf3b3d9ba6f
bugtype <bugtype.kr@gmail.com>
2020-07-26 22:14:42 +0900

    init

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,32 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Solomon on 6/15/20.
+//  Copyright © 2020 TechSolomon. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame <String> {
+        let emojis = ["👻", "🎃", "🕷"]
+        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    
+    // MARK: – Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
